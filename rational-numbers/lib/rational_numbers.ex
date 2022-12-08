5c70b12710fcad4ae923a210cc7b3a08cf080641
Berkan Unal <Berkanunal@gmail.com>
2022-12-08 20:26:41 +0300

    submit rational nums

--- rational-numbers/lib/rational_numbers.ex
+++ rational-numbers/lib/rational_numbers.ex
@@ -17,37 +17,33 @@ defmodule RationalNumbers do
   Multiply two rational numbers
   """
   @spec multiply(a :: rational, b :: rational) :: rational
-  def multiply({a_num, a_den}, {b_num, b_den}), do: { a_num * b_num, a_den * b_den } |> reduce
+  def multiply({a_num, a_den}, {b_num, b_den}), do: { a_num * b_num |> trunc, a_den * b_den |> trunc } |> reduce
 
   @doc """
   Divide two rational numbers
   """
   @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by(num, {den_num, den_den}) when den_den * den_num > 0, do: multiply(num, {den_den, den_num})
-  def divide_by(num, {den_num, den_den}) when den_den * den_num < 0 do
-    multiply(num, {Kernel.abs(den_den) * -1, Kernel.abs(den_num)})
-  end
+  def divide_by(num, {x, y}), do: multiply(num, {y, x})
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs({num, den}), do: {Kernel.abs(num), Kernel.abs(den)} |> reduce
+  def abs({x, y}), do: {Kernel.abs(x), Kernel.abs(y)} |> reduce
 
   @doc """
   Exponentiation of a rational number by an integer
   """
   @spec pow_rational(a :: rational, n :: integer) :: rational
   def pow_rational({a_num, a_den}, n) when n == 0, do: {1, 1}
-  def pow_rational({a_num, a_den}, n) when n > 0, do: {a_num ** n, a_den ** n}
-  def pow_rational({a_num, a_den}, n) when n < 0, do: {a_den ** (n * -1), a_num ** (n * -1)}
+  def pow_rational({a_num, a_den}, n) when n > 0, do: {a_num ** n, a_den ** n} |> fix_signs
+  def pow_rational({a_num, a_den}, n) when n < 0, do: {a_den ** (n * -1), a_num ** (n * -1)} |> fix_signs
 
   @doc """
   Exponentiation of a real number by a rational number
   """
   @spec pow_real(x :: integer, n :: rational) :: float
-  def pow_real(x, n) do
-  end
+  def pow_real(x, {a, b}), do: (x ** (a/b))
 
   @doc """
   Reduce a rational number to its lowest terms
@@ -55,6 +51,9 @@ defmodule RationalNumbers do
   @spec reduce(a :: rational) :: rational
   def reduce({x, y}) do
     gcd = Integer.gcd(x, y)
-    { x / gcd , y /gcd }
+    { x / gcd |> trunc, y /gcd |> trunc } |> fix_signs()
   end
+
+  defp fix_signs({x, y}) when y < 0, do: {x, y} |> multiply({-1, -1})
+  defp fix_signs(a), do: a
 end
