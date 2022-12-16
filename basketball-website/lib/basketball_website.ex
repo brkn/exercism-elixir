5e370f658245a54ca9a8049c40e6e9729e0d9464
Berkan Unal <Berkanunal@gmail.com>
2022-12-17 02:03:59 +0300

    Refactor basketball

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,8 +1,8 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
     path
-      |> String.split(".")
-      |> Enum.reduce(data, fn elem, acc -> if Kernel.is_nil(acc), do: nil, else: Map.get(acc, elem) end)
+    |> String.split(".")
+    |> Enum.reduce(data, fn elem, acc -> acc[elem] end)
   end
 
   def get_in_path(data, path), do: data |> Kernel.get_in(String.split(path, "."))
