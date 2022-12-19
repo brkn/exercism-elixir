dcf6408770fdb453a0d44aa0aa788e478eec2a76
Berkan Unal <Berkanunal@gmail.com>
2022-12-19 17:48:29 +0300

    upd

---
+++ lucas-numbers/lib/lucas_numbers.ex
@@ -0,0 +1,14 @@
+defmodule LucasNumbers do
+  @moduledoc """
+  Lucas numbers are an infinite sequence of numbers which build progressively
+  which hold a strong correlation to the golden ratio (φ or ϕ)
+
+  E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
+  """
+  def generate(count) when not is_integer(count) or count < 1,
+    do: raise(ArgumentError, message: "count must be specified as an integer >= 1")
+
+  def generate(count) do
+    Stream.unfold({2, 1}, fn {curr, next} -> {curr, {next, curr + next}} end) |> Enum.take(count)
+  end
+end
