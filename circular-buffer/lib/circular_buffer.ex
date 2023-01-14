85dcdf14f1eb967160755d07cf4062ea78f5c10d
Berkan Unal <Berkanunal@gmail.com>
2023-01-14 16:00:26 +0300

    Solve circular buffer, its a queue

--- circular-buffer/lib/circular_buffer.ex
+++ circular-buffer/lib/circular_buffer.ex
@@ -1,4 +1,6 @@
 defmodule CircularBuffer do
+  use GenServer
+
   @moduledoc """
   An API to a stateful process that fills and empties a circular buffer
   """
@@ -8,6 +10,7 @@ defmodule CircularBuffer do
   """
   @spec new(capacity :: integer) :: {:ok, pid}
   def new(capacity) do
+    GenServer.start_link(__MODULE__, capacity)
   end
 
   @doc """
@@ -15,6 +18,7 @@ defmodule CircularBuffer do
   """
   @spec read(buffer :: pid) :: {:ok, any} | {:error, atom}
   def read(buffer) do
+    GenServer.call(buffer, :read_oldest)
   end
 
   @doc """
@@ -22,6 +26,7 @@ defmodule CircularBuffer do
   """
   @spec write(buffer :: pid, item :: any) :: :ok | {:error, atom}
   def write(buffer, item) do
+    GenServer.call(buffer, {:write, item})
   end
 
   @doc """
@@ -29,6 +34,7 @@ defmodule CircularBuffer do
   """
   @spec overwrite(buffer :: pid, item :: any) :: :ok
   def overwrite(buffer, item) do
+    GenServer.call(buffer, {:overwrite, item})
   end
 
   @doc """
@@ -36,5 +42,42 @@ defmodule CircularBuffer do
   """
   @spec clear(buffer :: pid) :: :ok
   def clear(buffer) do
+    GenServer.call(buffer, :clear)
+  end
+
+  @impl GenServer
+  def init(capacity), do: {:ok, %{capacity: capacity, stack: []}}
+
+  @impl GenServer
+  def handle_call(:read_oldest, _from, %{stack: []} = state),
+    do: {:reply, {:error, :empty}, state}
+
+  def handle_call(:read_oldest, _from, %{stack: stack} = state) do
+    {value, new_stack} = List.pop_at(stack, -1)
+
+    {:reply, {:ok, value}, %{state | stack: new_stack}}
+  end
+
+  @impl GenServer
+  def handle_call({:write, _}, _from, %{stack: stack, capacity: cap} = state)
+      when length(stack) == cap,
+      do: {:reply, {:error, :full}, state}
+
+  def handle_call({:write, item}, _from, %{stack: stack} = state) do
+    {:reply, :ok, %{state | stack: [item | stack]}}
+  end
+
+  @impl GenServer
+  def handle_call(:clear, _from, state), do: {:reply, :ok, %{state | stack: []}}
+
+  @impl GenServer
+  def handle_call({:overwrite, item}, _from, %{stack: stack, capacity: cap} = state)
+      when length(stack) != cap,
+      do: {:reply, :ok, %{state | stack: [item | stack]}}
+
+  def handle_call({:overwrite, item}, _from, %{stack: stack} = state) do
+    {_, new_stack} = List.pop_at(stack, -1)
+
+    {:reply, :ok, %{state | stack: [item | new_stack]}}
   end
 end
